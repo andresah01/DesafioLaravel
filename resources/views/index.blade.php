5db4e0d411ba43f801290a0347e34c3921aa9e0d
andresah01 <andresah1999@gmail>
2023-01-12 11:45:13 -0500

    Proyecto de citas veterinarias con laravel

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,22 +5,30 @@
 @section('contenido')
     <div class="frames-container-application" style="display: grid; place-items: center;">
         <div class="heading-container">
-            <h1>Veterinaria laravel</h1>
+            <h1>Veterinaria Laravel</h1>
         </div>
         <div class="details-container">
-            <p> Clinica veterinaria laravel en la ciudad de Medellin. </p>
-            <p> Contamos con equipos nuevos de última tecnología y personal xapacitado para ofrecerte siempre el mejor servicio,
+            <p> Clinica veterinaria Laravel en la ciudad de Medellin. </p>
+            <p> Contamos con equipos nuevos de última tecnología y personal capacitado para ofrecerte siempre el mejor servicio,
                 dando una respuesta rápida y oportuna a las necesidades de tu mascota.
             </p>
             <p> Nuestro servicios </p>
-            <ul class="list-group mb-0">
+            {{-- <ul class="list-group mb-0">
                 <li class="list-group-item"> Consulta </li>
                 <li class="list-group-item"> Desparasitación </li>
                 <li class="list-group-item"> Vacunación </li>
                 <li class="list-group-item"> Imagenología </li>
-                <li class="list-group-item"> Peluqueria</li>
+                <li class="list-group-item"> Peluquería</li>
                 <li class="list-group-item">  Laboratorio </li>
-            </ul>
+            </ul> --}}
+            <div class="list-group">
+                <a href="{{route('cita.agendar')}}" class="list-group-item list-group-item-action"> Consulta </a>
+                <a href="{{route('cita.agendar')}}" class="list-group-item list-group-item-action"> Desparasitación </a>
+                <a href="{{route('cita.agendar')}}" class="list-group-item list-group-item-action"> Vacunación </a>
+                <a href="{{route('cita.agendar')}}" class="list-group-item list-group-item-action"> Imagenología </a>
+                <a href="{{route('cita.agendar')}}" class="list-group-item list-group-item-action"> Peluquería </a>
+                <a href="{{route('cita.agendar')}}" class="list-group-item list-group-item-action"> Laboratorio </a>
+            </div>
         </div>
         <br>
         <div class="details-container" style="display: grid; place-items: center;">
