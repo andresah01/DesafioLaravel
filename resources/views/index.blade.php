2b0d643661942a63cba8a6eed08237fceb75758d
andresah01 <andresah1999@gmail>
2023-01-12 11:45:48 -0500

    Proyecto de citas veterinarias con laravel

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,14 +13,6 @@
                 dando una respuesta rápida y oportuna a las necesidades de tu mascota.
             </p>
             <p> Nuestro servicios </p>
-            {{-- <ul class="list-group mb-0">
-                <li class="list-group-item"> Consulta </li>
-                <li class="list-group-item"> Desparasitación </li>
-                <li class="list-group-item"> Vacunación </li>
-                <li class="list-group-item"> Imagenología </li>
-                <li class="list-group-item"> Peluquería</li>
-                <li class="list-group-item">  Laboratorio </li>
-            </ul> --}}
             <div class="list-group">
                 <a href="{{route('cita.agendar')}}" class="list-group-item list-group-item-action"> Consulta </a>
                 <a href="{{route('cita.agendar')}}" class="list-group-item list-group-item-action"> Desparasitación </a>
